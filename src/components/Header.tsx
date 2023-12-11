bbb0c6f2ffa375328c05a60a6f3d1655245b7a6c
Lianny Coringa <lianny_costa@hotmail.com>
2023-12-11 15:32:46 -0300

    alterando o titulo para minusculo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar';
 function Header() {
   return (
     <header>
-      <h1 className="fs-1">ARCA DA SAGRADA ALIANÇA</h1>
+      <h1 className="fs-1">Arca da Sagrada Aliança</h1>
       <h2 className="fs-2">Movimento Cristão</h2>
       <NavBar />
     </header>
